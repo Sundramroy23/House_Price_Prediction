db2c8ea765b2dbec00e413597e414ed0c10522b9
Sundram Kumar <sundramroy1208@gmail.com>
2024-08-14 23:55:15 +0530

    Update app.py
    added host

--- app.py
+++ app.py
@@ -156,4 +156,4 @@ def predict():
 
 # Run the Flask application
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)  # Start the application in debug mode on port 5000
+    app.run(debug=True, port=5000,host='0.0.0.0')  # Start the application in debug mode on port 5000
